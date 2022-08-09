a9e72e691fa541443569f26339dbb7afbccc73a1
kmyobin <sowhat0516@gmail.com>
2022-08-09 23:32:44 +0900

    open api 가져오기

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,7 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+// 일별 박스오피스
+export const API_URL = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json'
+
+export const API_KEY = '80e186095be90d62a29c0bd57b96b844'
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,14 +1,54 @@
-import React from 'react'
-import { FaCode } from "react-icons/fa";
+import React, { useEffect, useState } from 'react'
+import {API_KEY, API_URL} from '../../Config';
 
 function LandingPage() {
+    
+    function getYesterday(){ // YYYYMMDD
+        var today = new Date();
+        var yesterday = new Date(today.setDate(today.getDate() - 1));
+
+        var year = today.getFullYear();
+        var month = ('0' + (today.getMonth() + 1)).slice(-2);
+        var day = ('0' + today.getDate()).slice(-2);
+
+        return year+month+day;
+    }
+
+    const yesterday = getYesterday();
+
+    const [Loading, setLoading] = useState(true);
+    const [Movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        var endpoint = `${API_URL}?key=${API_KEY}&targetDt=${yesterday}`
+        fetchMovies(endpoint)
+    }, [])
+
+    async function fetchMovies(endpoint) {
+        /*const response = await ( await fetch(endpoint)).json();
+        console.log(response);
+        setMovies(response.boxOfficeResult.dailyBoxOfficeList);
+        setLoading(false);*/
+        await fetch(endpoint)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response);
+            setMovies([...Movies, ...response.boxOfficeResult.dailyBoxOfficeList])
+            setLoading(false);
+        })
+    }
+
     return (
         <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+            <div>
+                {Loading ? <h1>Loading...</h1> :
+                    <div>{Movies.map(movie => 
+                    <div key={movie.movieCd}> <h3>{movie.rank}위 : {movie.movieNm}</h3> 
+                    <p>누적 관객 수 : {movie.audiAcc}명</p>
+                    <p>영화 개봉일 :  ({movie.openDt}) </p>
+                </div>)} </div>}
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
+            
         </>
     )
 }
