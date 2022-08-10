e7c336ea3a20faa089b293c65eecf66290d8b4b7
kmyobin <sowhat0516@gmail.com>
2022-08-10 23:29:24 +0900

    크롤링 실패

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -2,6 +2,10 @@
 export const USER_SERVER = '/api/users';
 
 // 일별 박스오피스
-export const API_URL = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json'
+export const P_API_URL_DAYBO = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json'
 
-export const API_KEY = '80e186095be90d62a29c0bd57b96b844'
+export const P_API_KEY = '80e186095be90d62a29c0bd57b96b844'
+
+export const N_ID_KEY = 'fY_F0i4T8WxRFdxvPqR4'
+
+export const N_SECRET_KEY = 'OtqyeKtXjx'
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import {API_KEY, API_URL} from '../../Config';
+import {P_API_URL_DAYBO, P_API_KEY, N_ID_KEY, N_SECRET_KEY} from '../../Config';
+import axios from 'axios';
 
 function LandingPage() {
     
@@ -7,36 +8,76 @@ function LandingPage() {
         var today = new Date();
         var yesterday = new Date(today.setDate(today.getDate() - 1));
 
-        var year = today.getFullYear();
-        var month = ('0' + (today.getMonth() + 1)).slice(-2);
-        var day = ('0' + today.getDate()).slice(-2);
+        var year = yesterday.getFullYear();
+        var month = ('0' + (yesterday.getMonth() + 1)).slice(-2);
+        var day = ('0' + yesterday.getDate()).slice(-2);
 
         return year+month+day;
     }
 
     const yesterday = getYesterday();
+    
 
     const [Loading, setLoading] = useState(true);
     const [Movies, setMovies] = useState([]);
+    const [Images, setImages] = useState(null);
 
     useEffect(() => {
-        var endpoint = `${API_URL}?key=${API_KEY}&targetDt=${yesterday}`
+        var endpoint = `${P_API_URL_DAYBO}?key=${P_API_KEY}&targetDt=${yesterday}`
         fetchMovies(endpoint)
     }, [])
 
-    async function fetchMovies(endpoint) {
-        /*const response = await ( await fetch(endpoint)).json();
-        console.log(response);
-        setMovies(response.boxOfficeResult.dailyBoxOfficeList);
-        setLoading(false);*/
-        await fetch(endpoint)
+    const fetchMovies = (endpoint) => {
+        fetch(endpoint)
         .then(response => response.json())
         .then(response => {
             console.log(response);
-            setMovies([...Movies, ...response.boxOfficeResult.dailyBoxOfficeList])
+            let temps=response.boxOfficeResult.dailyBoxOfficeList;
+            console.log(temps);
+            setMovies(...[temps]);
             setLoading(false);
+            console.log('before');
+            console.log(temps[0].movieCd)
+            
+            /*for(let i = 0; i< temps.length; i++){
+                console.log(temps[i].movieCd)
+            }*/
+            
+            console.log('after')
+
+            for(let i=0; i<temps.length; i++){
+                getSearchMovie(temps[i].movieCd)
+                console.log('after')
+            }
+           
         })
     }
+
+
+    function getSearchMovie(key) {
+        //const search = key;
+        //const {
+        //    data:{
+              let  items = axios.get(`https://openapi.naver.com/v1/search/movie.json`,
+        //    }} = 
+            {
+                params: {
+                    query : key,
+                    display : 20
+                },
+                headers: {
+                    'X-Naver-Client-Id': N_ID_KEY,
+                    'X-Naver-Client-Secret': N_SECRET_KEY
+                }       
+            });
+            setImages([...items]);
+            setLoading(false);
+            if(Images!=null)console.log(Images);
+            else console.log('error');
+        
+    }
+    
+    
 
     return (
         <>
@@ -45,12 +86,12 @@ function LandingPage() {
                     <div>{Movies.map(movie => 
                     <div key={movie.movieCd}> <h3>{movie.rank}위 : {movie.movieNm}</h3> 
                     <p>누적 관객 수 : {movie.audiAcc}명</p>
-                    <p>영화 개봉일 :  ({movie.openDt}) </p>
+                    <p>영화 개봉일 :  {movie.openDt} </p>
                 </div>)} </div>}
             </div>
             
         </>
     )
-}
+};
 
 export default LandingPage
